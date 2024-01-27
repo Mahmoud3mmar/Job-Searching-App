4bd4da408c4f61ae9d26f93fac422a0db31708cc
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2024-01-27 19:47:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from 'express'
 import ConnectToDB from './db/dbconnection.js'
 import v1Router from './src/Routers/v1.routes.js'
 import userRouter from './src/modules/users/routers/user.router.js'
-import postRouter from './src/modules/posts/routers/posts.router.js'
+import CompanyRouter from './src/modules/Company/routers/company.router.js'
+import jobRouter from './src/modules/job/routers/job.router.js'
+import ApplicationRouter from './src/modules/application/routers/application.router.js'
+
 import  dotenv  from 'dotenv'
 
 
@@ -10,23 +13,23 @@ import  dotenv  from 'dotenv'
 dotenv.config()
 const app = express()
 const port = 3000
-import {v2 as cloudinary} from 'cloudinary';
-          
-          
-cloudinary.config({ 
-  cloud_name: 'dhdkmq1q8', 
-  api_key: '739591961255271', 
-  api_secret: 'DyjJGA2p1a--l7nuhH5kBaDd_zg' 
-});
+
 
 
 app.use(express.json())
 app.use(express.static('uploads'))
 app.use('/users',userRouter)
-app.use('/posts',postRouter)
+app.use('/jobs',jobRouter)
+app.use('/companies',CompanyRouter)
+
+app.use('/applications/:companyId/:date',ApplicationRouter)
+
+
 
 app.use('/api/v1',v1Router)
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
 
 
 
